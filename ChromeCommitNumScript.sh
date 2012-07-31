084591e849ef2bc0201d7a60ac1b8f9b5ded31e7
akhnaifer <akhnaifer@gmail.com>
2012-07-31 02:35:33 -0400

    Fixed both scripts to remove CR '/r' from the end of each line… now its working

--- ChromeCommitNumScript.sh
+++ ChromeCommitNumScript.sh
@@ -3,11 +3,18 @@
 # and Numcontributors
 # Script by Abdulrhman Alkhanifer, 7-30-2012
 
+#set -x verbose #echo on
+
 file="myfilelist.txt"
+#cat $file | od -c
+#${filename/$'\n'/} to remove '\n' from the filename
 
 while IFS= read -r filename
 do
-echo $filename
+#echo $string1 | tr "\n" " "
+var="$(echo ${filename/$'\r'/})"
+#var="$(echo $filename | grep \r )"
+
 # display $line or do somthing with $line
-x= git log --since='2008-01-01' --until='2010-12-3' --oneline -- ${filename} |wc -l
+git log --since='2008-01-01' --until='2010-12-3' --oneline -- ${var} |wc -l
 done <"$file"
--- ChromeContributorsNum.sh
+++ ChromeContributorsNum.sh
@@ -7,6 +7,7 @@ file="myfilelist1.txt"
 while IFS= read -r filename
 do
 # display $line or do somthing with $line
-git log --since='2008-01-01' --until='2010-12-3' --pretty=format:%ae -- $filename | sort -u | wc -l
+var="$(echo ${filename/$'\r'/})"
+git log --since='2008-01-01' --until='2010-12-3' --pretty=format:%ae -- $var | sort -u | wc -l
 
 done <"$file"
